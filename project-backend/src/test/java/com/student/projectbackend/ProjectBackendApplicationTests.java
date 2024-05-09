dbd37e634f8c5179e74bfa2411a6ef608602b430
Wendelsena <wendel4019@gmail.com>
2024-05-09 18:21:25 -0300

    First commit

---
+++ project-backend/src/test/java/com/student/projectbackend/ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
